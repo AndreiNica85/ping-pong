6f5983ddecb3c18712095c22160e421825e3c8ce
Nica <andrei.nica@solera.com>
2022-10-19 14:22:29 +0200

    1st commit

--- src/test/java/com/pingpong/solera/todoapp/TodoAppApplicationTests.java
+++ src/test/java/com/pingpong/solera/todoapp/TodoAppApplicationTests.java
@@ -1,13 +1,22 @@
 package com.pingpong.solera.todoapp;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import com.pingpong.solera.todoapp.model.Note;
 
 @SpringBootTest
 class TodoAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Note note = new Note(1, "Do", "some text in here", LocalDate.now(), LocalDate.now(), LocalDate.now(), "http://localhost:8080/notes", 1);
+		
+		
+		Assert.isNull(null, note.getDescr());
 	}
 
 }
